a32b15775890c73d9e8591e3c6b9ade2d76b64b1
Mathieu <albatros69+github@gmail.com>
2016-11-25 00:43:14 +0100

    remplacement print -> logging

--- purge_lbc.py
+++ purge_lbc.py
@@ -3,6 +3,14 @@
 
 import requests, sqlite3 #, re, os
 from time import sleep
+import logging, logging.handlers
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.handlers.SysLogHandler(facility=logging.handlers.SysLogHandler.LOG_USER, address='/dev/log')
+formatter = logging.Formatter('%(module)s: %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 conn = sqlite3.connect('lbc.sqlite')
 curs = conn.cursor()
@@ -10,23 +18,27 @@ curs = conn.cursor()
 if __name__ == '__main__':
 
     curs.execute("SELECT COUNT(*) FROM offers_seen;")
-    print curs.fetchone()[0], "annonces"
+    logger.info( "%d annonces" % (curs.fetchone()[0], ))
     curs.execute("DELETE FROM offers_seen WHERE date < DATETIME('now', '-61 day');")
-    print curs.rowcount, "annonces de plus de 2 mois supprimées"
+    logger.info("%d annonces de plus de 2 mois supprimées" % (curs.rowcount, ) )
+
     offers_deleted = []
-    for i, u in curs.execute("SELECT id, link FROM offers_seen WHERE date < DATETIME('now', '-10 day');"):
+    c = 0
+    for i, u in curs.execute("SELECT id, link FROM offers_seen WHERE date < DATETIME('now', '-31 day');"):
+        c += 1
         try:
             r = requests.head(u, timeout=2)
             #print i, u, r.status_code
         except:
             sleep(2)
             continue
-        if r.status_code != 200:
-            offers_deleted.append(str(i))
-        sleep(0.5)
+        else:
+            if r.status_code != 200:
+                offers_deleted.append(str(i)) 
+            sleep(0.5)
 
     curs.execute("DELETE FROM offers_seen WHERE id in (%s);" % (",".join(offers_deleted), ));
-    print len(offers_deleted), "annonces introuvables supprimées"
+    logger.info("%d annonces introuvables supprimées parmi %d annonces de plus d'1 mois" % (len(offers_deleted), c) )
     curs.execute("VACUUM;")
     conn.commit()
     conn.close()
