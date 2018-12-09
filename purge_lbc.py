f4b7990d84af9657286dd2aa84d115ab6688c6b8
Mathieu <albatros69+github@gmail.com>
2018-12-09 12:13:37 +0100

    LBC envoie le code 200 systématiquement désormais

--- purge_lbc.py
+++ purge_lbc.py
@@ -22,24 +22,7 @@ if __name__ == '__main__':
     curs.execute("DELETE FROM offers_seen WHERE date < DATETIME('now', '-61 day');")
     logger.info("%d annonces de plus de 2 mois supprimées" % (curs.rowcount, ) )
 
-    offers_deleted = []
-    c = 0
-    for i, u in curs.execute("SELECT id, link FROM offers_seen WHERE date < DATETIME('now', '-31 day');"):
-        c += 1
-        try:
-            r = requests.head(u, timeout=2)
-            #print i, u, r.status_code
-        except:
-            sleep(2)
-            continue
-        else:
-            if r.status_code != 200:
-                offers_deleted.append(str(i)) 
-            sleep(0.5)
-
-    curs.execute("DELETE FROM offers_seen WHERE id in (%s);" % (",".join(offers_deleted), ));
-    logger.info("%d annonces introuvables supprimées parmi %d annonces de plus d'1 mois" % (len(offers_deleted), c) )
-    curs.execute("VACUUM;")
     conn.commit()
+    curs.execute("VACUUM;")
     conn.close()
 
